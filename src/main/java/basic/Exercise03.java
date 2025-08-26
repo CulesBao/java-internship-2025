798aa5676c3d1d6096aea680f5c8bb8f71a95d65
Bảo Cules <149091475+CulesBao@users.noreply.github.com>
2025-08-26 10:27:32 +0700

    chore fix
    Co-authored-by: Mẫn <vanman.lqd@gmail.com>

--- src/main/java/basic/Exercise03.java
+++ src/main/java/basic/Exercise03.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Exercise03 {
     public static String primeFactorization(int n) {
-        List<Integer> factors = new java.util.ArrayList<>();
+        final List<Integer> factors = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
                 factors.add(i);
