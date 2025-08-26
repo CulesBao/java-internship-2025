f6ee25b75e79cf3aefbf8add37d03ccae3fff40a
CulesBao <102230334@sv1.dut.udn.vn>
2025-08-26 10:15:51 +0700

    feat: add exercise 03

---
+++ src/main/java/basic/Exercise03.java
@@ -0,0 +1,16 @@
+package src.main.java.basic;
+
+import java.util.List;
+
+public class Exercise03 {
+    public static String primeFactorization(int n) {
+        List<Integer> factors = new java.util.ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return String.join(" * ", factors.stream().map(String::valueOf).toArray(String[]::new));
+    }
+}
