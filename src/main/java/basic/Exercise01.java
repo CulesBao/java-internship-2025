04f93f32f389814262e0b7efd0fb84d424179968
CulesBao <102230334@sv1.dut.udn.vn>
2025-08-26 10:18:22 +0700

    fix: chore fix

--- src/main/java/basic/Exercise01.java
+++ src/main/java/basic/Exercise01.java
@@ -2,6 +2,15 @@ package src.main.java.basic;
 
 public class Exercise01 {
   public int gcd(int x, int y) {
+    if (x == 0) {
+      return y;
+    }
+    if (y == 0) {
+      return x;
+    }
+    if (x == y) {
+      return y;
+    }
     return gcd(y, x % y);
   }
 
