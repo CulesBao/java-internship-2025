886f6beb9c3ce25ef974fc06882aeede8db5e878
CulesBao <102230334@sv1.dut.udn.vn>
2025-08-28 10:19:52 +0700

    fix: add final to instance

--- src/test/java/string/Exercise02Test.java
+++ src/test/java/string/Exercise02Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class Exercise02Test {
   @Test
   public void shouldReturnSumOfNumbersInString() {
-    Exercise02 exercise02 = new Exercise02();
+    final Exercise02 exercise02 = new Exercise02();
     assertEquals(123, exercise02.sumInString("abc123xyz"));
     assertEquals(44, exercise02.sumInString("aa11b33"));
     assertEquals(18, exercise02.sumInString("7 11"));
